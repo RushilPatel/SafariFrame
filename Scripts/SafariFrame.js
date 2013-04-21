c4125f9c2c83676b2d077f04e2439d5c7fad8dcd
Rushil Patel <rushilpatel2806@hotmail.com>
2013-04-20 22:31:58 -0400

    First Commit

---
+++ Scripts/SafariFrame.js
@@ -0,0 +1,41 @@
+﻿
+function goBack()
+  {
+  window.history.back()
+  }
+function goForward()
+  {
+  window.history.forward()
+  }
+ function urlKeyPress(e)
+    {
+        if (e.keyCode == 13)
+        {
+            var url = document.getElementById('urlbox').value;
+            if(url != ""){
+            document.getElementById('browserframe').src = "http://"+url;
+            }
+            
+        }
+    }
+  function setURL()
+  {	
+  		document.getElementById('urlbox').value=document.getElementById('browserframe').src;	
+  }
+  function searchKeyPress(e)
+    {
+        if (e.keyCode == 13)
+        {
+            var key = document.getElementById('googlebox').value;
+            if (key != ""){
+            document.location = 'http://www.google.com/search?q='+key;
+            }  
+        }
+    }
+   function searchEvent()
+    {
+        var key = document.getElementById('googlebox').value;
+            if (key != ""){
+            document.location = 'http://www.google.com/search?q='+key;
+            }
+    }
